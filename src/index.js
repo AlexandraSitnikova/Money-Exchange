289f3069ac313ab73769814d7e38b49fc3e75c5e
Shavel, Ihar <IShavel@slb.com>
2018-09-12 14:28:28 +0300

    exchangeCoins implement

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
-// PLEASE DON'T change function name
+
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if(currency <= 0) {
+        return {};
+    } else if(currency > 10000) {
+        let errObj = {error: "You are rich, my friend! We don't have so much coins for exchange"};
+        return errObj;
+    } else {
+        let coins = [50, 25, 10, 5, 1];
+        let units = ["H", "Q", "D", "N", "P"];
+        let amounts = [0, 0 ,0, 0, 0];
+
+        coins.forEach(function(coin, index) {
+            while(currency >= coin) {
+                amounts[index]++;
+                currency -= coin;
+            };
+        });
+        
+        let resObj = {};
+        amounts.forEach(function(amount, index) {
+            if(amount !== 0) {
+                resObj[units[index]] = amount;
+            }
+        });
+
+        return resObj;
+    }
 }
